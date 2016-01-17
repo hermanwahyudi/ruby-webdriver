02f4fac6c02cdedcc9ab6e34b298b640454fa081
Herman Wahyudi <herman.wahyudi02@gmail.com>
2016-01-17 11:00:52 +0700

    Test Jenkins #2

--- test.login.tkpd.rb
+++ test.login.tkpd.rb
@@ -1,17 +1,4 @@
 require 'rubygems'
 require 'selenium-webdriver'
 
-driver = Selenium::WebDriver.for :firefox
-driver.get "https://www.tokopedia.com/"
-
-element = driver.find_element :name => "q"
-element.send_keys "Cheese!"
-element.submit
-
-puts "Page title is #{driver.title}"
-
-wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-wait.until { driver.title.downcase.start_with? "cheese!" }
-
-puts "Page title is #{driver.title}"
-driver.quit 
+puts "Test Jenkins"
